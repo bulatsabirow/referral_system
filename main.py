dc10fdd151954e3df01f00786e6150ec5de7d8fc
bulatsabirow <bulatsabirow@gmail.com>
2024-03-13 16:50:47 +0300

    + base user authentication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from auth import fastapi_users
+from auth import fastapi_users, auth_backend
 from auth.schema import UserRead, UserCreate
 
 app = FastAPI(title="Stakewolle")
@@ -9,4 +9,9 @@ app.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="/auth",
     tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/jwt/login",
+    tags=["auth"]
 )
